2ad49ce52d99652dcf654c3b6bd911899150dd19
JoshuaSG3161 <32961794+JoshuaSG3161@users.noreply.github.com>
2019-01-26 21:30:10 -0500

    Add files via upload

---
+++ 10.21CLASS.py
@@ -0,0 +1,51 @@
+class ResonantCircuit:
+    def __init__(w, b, k):
+        self._w = w
+        self._b = b
+        self._k = k
+        
+    def getw():
+        return self._w
+    
+    def setw(w):
+        self._w = w
+        return self._w
+        
+    def getb():
+        return self._b
+    
+    def setw(b):
+        self._b = b
+        return self._b
+        
+    def getk():
+        return self._w
+    
+    def setk(k):
+        self._k = k
+        return self._k
+        
+    def display():
+        print()
+        
+class SeriesResonantCircuit(ResonantCircuit):
+    def __init__():
+        super()
+        
+    def display():
+        print()
+
+class ParallelResonanceCircuit(ResonantCircuit):
+    def __init__():
+        super()
+        
+    def Par_EQN:
+        self._R = self._k
+        self.C = (1/(self._b * self._R))
+        #If self._R doesnt work, use self._k
+        self._L = (1/((self._w**2)*(self._C)))
+        return self._L and self._C
+        
+        
+    def display():
+        print()
